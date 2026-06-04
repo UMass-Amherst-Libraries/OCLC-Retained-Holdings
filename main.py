8fc6d1d1a0402de351e248fea259e5f2290f6b90
ameliasutton <ameliasutton@umass.edu>
2026-06-04 07:48:33 -0400

    Updated UI theme. All columns from input CSV are carried over to output.

--- main.py
+++ main.py
@@ -3,24 +3,28 @@ import pandas as pd
 import dotenv
 import os
 import tkinter as tk
+from tkinter import ttk
 from tkinter import filedialog
+import sv_ttk
 import sys
 import aiohttp
 import asyncio
 from tqdm import tqdm 
 import logging
 from datetime import datetime
+import numpy as np
 
 # Popup Windows to give alert notices
 class PopupWindow:
     def __init__(self, text):
         self.popup = tk.Tk()
+        self.popup.attributes("-topmost", True)
         self.popup.wm_title("Popup Notice")
         self.popup.columnconfigure(0, minsize=100)
         self.popup.rowconfigure([0, 1], minsize=25)
 
 
-        self.text_label = tk.Label(master=self.popup, text=text)
+        self.text_label = ttk.Label(master=self.popup, text=text)
         self.text_label.grid(row=0, column=0)
         self.close_button = tk.Button(master=self.popup, text="OK", command=self.close)
         self.close_button.grid(row=1, column=0)
@@ -35,19 +39,23 @@ class Menu:
         self.act_menu = tk.Tk()
         self.act_menu.wm_title("Get OCLC Retained Holdings")
 
+        self.act_menu.configure(background="lavender")
+        sv_ttk.set_theme("light")
+
     # File Select Button
         self.select = tk.Button(master=self.act_menu, text="Select .csv file", command=self.file_select, font='TkDefaultFont 10')
         #self.run.grid(row=4, column=1, columnspan=1)
-        self.select.pack(side='top')
+        self.select.pack(side='top', pady=5)
 
     # Selected File Name
-        self.selectedFile = tk.Label(master=self.act_menu, text="Selected File: ", font='TkDefaultFont 10')
-        self.selectedFile.pack(side='top')
+        self.selectedFile = ttk.Label(master=self.act_menu, text="No file selected", font='TkDefaultFont 10')
+        self.selectedFile.configure(background="lavender")
+        self.selectedFile.pack(side='top', pady=5, padx=10)
 
     # Get OCLC Retained Holdings Button
-        self.run = tk.Button(master=self.act_menu, text="Retrieve and Save OCLC Retained Holdings", command=self.get_OCLC_retained_holdings, font='TkDefaultFont 10', state="disabled")
+        self.run = tk.Button(master=self.act_menu, text="Retrieve and Save OCLC Retained Holdings", command=self.get_OCLC_retained_holdings, font='TkDefaultFont 10 bold', state="disabled")
         #self.run.grid(row=4, column=1, columnspan=1)
-        self.run.pack(side='top', pady=10)
+        self.run.pack(side='top', padx=10, pady=5)
         self.filename = ''
 
     # Menu Bar
@@ -64,20 +72,33 @@ class Menu:
     def file_select(self):
         logging.info("File select clicked")
         self.filename = filedialog.askopenfilename()
-        self.selectedFile.config(text = f"Selected File: {self.filename[self.filename.rfind('/')+1:]}")
+        if self.filename =='':
+            self.selectedFile.config(text = f"No file selected")
+            self.run.config(state="disabled")
+        else:
+            self.selectedFile.config(text = f"Selected File: {self.filename[self.filename.rfind('/')+1:]}")
+            self.run.config(state="normal")
         self.selectedFile.update_idletasks()
-        self.run.config(state="normal")
 
     def get_OCLC_retained_holdings(self):
         logging.info("Get OCLC Retained Holdings clicked")
         oclc_numbers = readInputFile(self.filename)
         chunked_oclc_numbers = chunkList(oclc_numbers)
 
-        retained_holdings = []
-        for i, chunk in enumerate(chunked_oclc_numbers, start=1):
+        for (i, chunk) in chunked_oclc_numbers:
             logging.info(f'Retrieving retained holdings for chunk {i}')
+
+            oclc_df = pd.DataFrame(columns=['oclcNumber','title', 'allSymbols', 'allNames', 'notes'], index=range(0,len(chunk.index)), data=[]*100)
+            
             chunk_retained_holdings = asyncio.run(asyncGetRetainedHoldings(chunk))
-            retained_holdings += chunk_retained_holdings
+            for index, row in enumerate(chunk_retained_holdings):
+                oclc_df.iloc[index] = pd.Series(row)
+            chunk = pd.merge(chunk,oclc_df,on='oclcNumber', how='inner')
+            if i==0:
+                chunks = [chunk]
+            else:
+                chunks.append(chunk)
+        retained_holdings = pd.concat(chunks)
         saveResults(retained_holdings, self.filename)
 
 def getToken():
@@ -96,29 +117,29 @@ def readInputFile(filename):
         oclc_df = pd.read_csv(filename, dtype="string")
         if 'oclcNumber' not in oclc_df.columns:
             logging.warning('CSV must have column named oclcNumber.')
-            raise ValueError('CSV must have column named oclcNumber.')
+            PopupWindow('CSV must have column named oclcNumber.')
     elif filename[filename.rfind('.'):] == '.tsv':
         oclc_df = pd.read_csv(filename, dtype="string", delimiter = '\t')
         if 'oclcNumber' not in oclc_df.columns:
             logging.warning('CSV must have column named oclcNumber.')
-            raise ValueError('CSV must have column oclcNumber.')
+            PopupWindow('CSV must have column oclcNumber.')
     else:
         PopupWindow("File type must be .csv or .tsv")
-
-    oclc_numbers = oclc_df['oclcNumber'].dropna().astype(str).tolist()
+    
+    #oclc_numbers = oclc_df['oclcNumber'].dropna().astype(str).tolist()
     logging.info("Reading successful, OCLC numbers retrieved.")
-    return oclc_numbers
+    return oclc_df
 
 def chunkList(oclc_numbers):
-    chunk_size = 10000
+    chunk_size = 1000
     logging.info(f"Splitting list of OCLC numbers into chunks of {chunk_size} numbers.")
-    split_list = []
-    for i in range(0, len(oclc_numbers), chunk_size):
-        split_list.append(oclc_numbers[i:i+chunk_size])
-    logging.info(f"List split into {len(split_list)} chunks.")
-    return split_list
+    
+    split_dfs = oclc_numbers.groupby(np.arange(len(oclc_numbers.index))//chunk_size)
 
-async def asyncGetRetainedHoldings(oclc_numbers):
+    logging.info(f"List split into {split_dfs.ngroups} chunks.")
+    return split_dfs
+
+async def asyncGetRetainedHoldings(oclc_numbers:pd.DataFrame):
     async_conn = aiohttp.TCPConnector(limit=100) # defines the number of threads to use
     async_time = aiohttp.ClientTimeout(total=100000)
     
@@ -127,11 +148,15 @@ async def asyncGetRetainedHoldings(oclc_numbers):
     headers = {'Authorization': f'Bearer {getToken()}', 'Accept': 'application/json'}
     
     tasks = []
-    async with aiohttp.ClientSession(connector=async_conn, timeout=async_time) as session:
-        for ocn in oclc_numbers:
-            params={'oclcNumber': ocn}
+    session = aiohttp.ClientSession(connector=async_conn, timeout=async_time)
+    for row in oclc_numbers.index:
+        if pd.isna(oclc_numbers.loc[row]['oclcNumber']):
+            continue
+        else:
+            params={'oclcNumber': oclc_numbers.loc[row]['oclcNumber']}
             tasks.append(asyncio.create_task(asyncRequest(session=session, url=url, headers=headers, params=params)))
-        retained_holdings = [await f for f in tqdm(asyncio.as_completed(tasks), total=len(tasks))]
+    retained_holdings = [await f for f in tqdm(asyncio.as_completed(tasks), total=len(tasks))]
+    await session.close()
     return retained_holdings
 
 async def asyncRequest(session, url, headers, params):
@@ -162,15 +187,14 @@ async def processResponse(response, params):
                     for ih in inst:
                         symbols.append(ih.get('oclcSymbol', ih.get('symbol', '')))
                         names.append(ih.get('institutionName', ih.get('name', '')))
-                data = {'oclcNumber': params['oclcNumber'], 'title': title, 'allSymbols': '|'.join(filter(None, symbols)), 'allNames': ' | '.join(filter(None, names))}
+                data = {'oclcNumber': params['oclcNumber'], 'title': title, 'allSymbols': '|'.join(filter(None, symbols)), 'allNames': ' | '.join(filter(None, names)), 'notes': ''}
     return data
 
 
-def saveResults(retained_holdings, input_file_name):
+def saveResults(out_df, input_file_name):
     out_name = f"Output/Retained Holdings - {input_file_name[input_file_name.rfind('/')+1:input_file_name.rfind('.')]}.csv"
     logging.info(f"Saving results to {out_name}")
-    out_df = pd.DataFrame(retained_holdings)
-    out_df.to_csv(out_name, index=False)
+    out_df.drop_duplicates().to_csv(out_name, index=False)
     out_df.head()
     logging.info("Results saved!")
     PopupWindow("Results Saved!")
